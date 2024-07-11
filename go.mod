06c1db1ff7d39da5aae0aa24f3af98658d521032
Arif Yuniarto Fajar Bugaresa <arifyuniartofajarbugaresa@gmail.com>
2024-07-11 14:27:08 +0700

    feat: protobuf

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-learning
 
 go 1.22.4
+
+require google.golang.org/protobuf v1.34.2
--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-learning/model"
+	"google.golang.org/protobuf/encoding/protojson"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var user1 = &model.User{
+		Id:       "u001",
+		Name:     "Sylvana Windrunner",
+		Password: "Password123",
+		Gender:   model.UserGender_FEMALE,
+	}
+
+	var userList = &model.UserList{
+		List: []*model.User{
+			user1,
+		},
+	}
+
+	var garage1 = &model.Garage{
+		Id:   "g001",
+		Name: "Kalimdor",
+		Coordinate: &model.GarageCoordinate{
+			Latitude:  23.2222222,
+			Longitude: 24.33333,
+		},
+	}
+
+	var garageList = &model.GarageList{
+		List: []*model.Garage{
+			garage1,
+		},
+	}
+
+	var garageListByUser = &model.GarageListByUser{
+		List: map[string]*model.GarageList{
+			user1.Id: garageList,
+		},
+	}
+
+	fmt.Println(user1)
+	fmt.Println(user1.String())
+
+	fmt.Println(garageListByUser)
+	fmt.Println(userList)
+
+	jsonb, _ := protojson.Marshal(garageList)
+
+	fmt.Println(string(jsonb))
 }
